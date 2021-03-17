e3ef75345b6fdd4e3b872213393821d66a33bf22
Rijoanul Hasan Shanto <rijoanul.shanto@gmail.com>
2021-03-17 18:35:04 +0600

    [ADD] - boolean variable 'isHuman' with type

--- 02_vars/main.go
+++ 02_vars/main.go
@@ -5,13 +5,16 @@ import "fmt"
 func main() {
 	var name = "Robert Junior"
 	var age = 26
+	var isHuman = true
 
 	const PI = 3.1415
 
 	name += " #"
 	age = 25
+	isHuman = false
 
 	fmt.Printf("%s is a: '%T'\n", name, name)
 	fmt.Printf("%d is a: '%T'\n", age, age)
 	fmt.Printf("%f is a: '%T'\n", PI, PI)
+	fmt.Printf("%t is a: '%T'\n", isHuman, isHuman)
 }
