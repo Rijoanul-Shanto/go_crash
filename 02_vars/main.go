e92f2c7061624cbc5966055f39493f9c4041a03c
Rijoanul Hasan Shanto <rijoanul.shanto@gmail.com>
2021-03-17 18:33:43 +0600

    [ADD] - constant variable 'PI' with type

--- 02_vars/main.go
+++ 02_vars/main.go
@@ -6,6 +6,12 @@ func main() {
 	var name = "Robert Junior"
 	var age = 26
 
+	const PI = 3.1415
+
+	name += " #"
+	age = 25
+
 	fmt.Printf("%s is a: '%T'\n", name, name)
 	fmt.Printf("%d is a: '%T'\n", age, age)
+	fmt.Printf("%f is a: '%T'\n", PI, PI)
 }
