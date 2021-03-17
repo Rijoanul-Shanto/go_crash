f0fbd877d7ce473493cbfddb508c66485619aa48
Rijoanul Hasan Shanto <rijoanul.shanto@gmail.com>
2021-03-17 18:30:58 +0600

    [ADD] - integer variable 'age' with type

--- 02_vars/main.go
+++ 02_vars/main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	var name = "Robert Junior"
+	var age = 26
 
-	fmt.Printf("%s is a: '%T'", name, name)
+	fmt.Printf("%s is a: '%T'\n", name, name)
+	fmt.Printf("%d is a: '%T'\n", age, age)
 }
